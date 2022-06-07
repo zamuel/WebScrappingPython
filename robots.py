f25b2de45873ccc7dbcb29305cf95eec3ade5d02
Samuel <zamuel@ciencias.unam.mx>
2022-06-07 17:40:34 -0500

    Commit

--- robots.py
+++ robots.py
@@ -6,16 +6,21 @@ from urllib import robotparser
 robot_parser = robotparser.RobotFileParser()
 
 
+# We read de robots.txt of the website
 def prepare(robots_txt_url):
+    # Sets the URL referring to a robots.txt file.
     robot_parser.set_url(robots_txt_url)
+    # Reads the robots.txt URL and feeds it to the parser.
     robot_parser.read()
 
 
+# Function that let us know if we can make scrap on a determinate URL
 def is_allowed(target_url, user_agent='*'):
+    # Returns True if the useragent is allowed to fetch the url.
     return robot_parser.can_fetch(user_agent, target_url)
 
 
 if __name__ == '__main__':
-    prepare('http://www.apress.com/robots.txt')
-    print(is_allowed('http://www.apress.com/covers/'))
-    print(is_allowed('http://www.apress.com/gp/python'))
+    prepare('https://www.apress.com/robots.txt')
+    print(is_allowed('https://www.apress.com/covers/'))
+    print(is_allowed('https://www.apress.com/gp/python'))
